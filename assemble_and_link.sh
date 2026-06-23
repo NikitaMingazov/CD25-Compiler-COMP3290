c445913a7bc73594e80983492b1c75921876d1d4
Nikita Mingazov <NikitaMingazov@protonmail.com>
2026-06-23 10:41:56 +1000

    moved linker script out of src

--- assemble_and_link.sh
+++ assemble_and_link.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# script to get a binary from the .s produced by the x86_64 backend
 set -e
 
 if [ $# -ne 2 ]; then
@@ -8,6 +9,7 @@ fi
 
 nasm -f elf64 -g -F dwarf "$1" -o a.o
 cc -o "$2" -g a.o -no-pie
+# the direct linker usage is as follows, but using gcc as linker is simpler
 # ld -g -o "$2" a.o -lc \
 #    --dynamic-linker /lib64/ld-linux-x86-64.so.2 \
 #    -no-pie
